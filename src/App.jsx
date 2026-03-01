64ceb1e00a85c3defbcc0e3d90e38a02e9cccd97
abhay721 <abhaysharma3294@gmail.com>
2026-03-01 23:50:06 +0530

    structure commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const App = () => {
+  return (
+ <>
+<p className='fs-5'>
+  Lorem ipsum dolor sit amet.
+</p>
+ </>
+  )
+}
+
+export default App
